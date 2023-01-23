94e779d747f99721bb52dbaab2d44e87c86589b7
jdacodes <105356546+jdacodes@users.noreply.github.com>
2023-01-23 19:51:35 +0800

    Added Logging Interceptor to NetworkService.kt

--- app/src/main/java/com/jdacodes/sampleanimelist/network/NetworkService.kt
+++ app/src/main/java/com/jdacodes/sampleanimelist/network/NetworkService.kt
@@ -1,20 +1,33 @@
 package com.jdacodes.sampleanimelist.network
 
+import com.jdacodes.sampleanimelist.BuildConfig
 import com.jdacodes.sampleanimelist.model.Data
 import com.jdacodes.sampleanimelist.model.NetworkResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 class NetworkService {
+    val BASE_URL = "https://api.jikan.moe"
+
+    val logging = HttpLoggingInterceptor().apply {
+        if (BuildConfig.DEBUG) {
+            this.setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            this.setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
+    }
+
+    val client = OkHttpClient.Builder().addInterceptor(logging)
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://api.jikan.moe")
-        .client(OkHttpClient())
+        .baseUrl(BASE_URL)
+        .client(client.build())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
